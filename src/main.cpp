04cd00f991316c7185c50db2c35cccc603a6db72
kristmakevel <kristmakevel@gmail.com>
2025-02-18 20:57:03 +0300

    Initial commit: CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int add(int a, int b);
+int subtract(int a, int b);
+
+int main() {
+    int a = 10;
+    int b = 3;
+
+    std::cout << "adding: " << add(a, b) << std::endl;
+    std::cout << "substracting: " << subtract(a, b) << std::endl;
+
+    return 0;
+}
