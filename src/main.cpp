8c33e803b609f31f5f0d6894338a0c8f04327788
kristmakevel <kristmakevel@gmail.com>
2025-02-18 21:15:42 +0300

    fix: Update main output message in main branch

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@ int main() {
     int a = 10;
     int b = 3;
 
-    std::cout << "adding: " << add(a, b) << std::endl;
-    std::cout << "substracting: " << subtract(a, b) << std::endl;
+    std::cout << "changed !! adding: " << add(a, b) << std::endl;
+    std::cout << "changed !! substracting: " << subtract(a, b) << std::endl;
 
     return 0;
 }
